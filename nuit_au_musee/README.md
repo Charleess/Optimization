e4d3435d557032301c9c9d04451ddaf140fd5ab4
Charles Ferault <charles@wst.local>
2018-02-01 13:55:41 +0100

    New formula for the museum

--- nuit_au_musee/README.md
+++ nuit_au_musee/README.md
@@ -15,14 +15,20 @@ $ python nuit_au_musee.py
 
 Après de nombreux essais infructeux, on utilisera l'intégration Python de SCIP pour résoudre le problème, ZIMPL étant trop obscur pour mettre en place la modélisation désirée. Les fichiers d'origine ZIMPL avec le début de raisonnement sont laissés à titre indicatif.
 
-L'utilisation de SCIP non installé à la racine peut demander de préciser l'endroit où est situé la librarie dynamique. SUr MacOS, `export DYLD_FALLBACK_LIBRARY_PATH=PATH/TO/YOUR/SCIP/lib` fait l'affaire.
+L'utilisation de SCIP non installé à la racine peut demander de préciser l'endroit où est situé la librarie dynamique. Sur MacOS, `export DYLD_FALLBACK_LIBRARY_PATH=PATH/TO/YOUR/SCIP/lib` fait l'affaire.
 
-L'idée générale va être de restreindre dramatiquement l'espace des possibilités d'implantation des caméras en imposant qu'elles aient au minimum 2 oeuvres d'art sur leur rayon maximal. Pour obtenir ce résultat, on choisit les implantations possibles à partir des positions des oeuvres, en tracant pour chaque paire d'oeuvres d'art un cercle qui passe par les deux points, et en prenant les deux centres obtenus.
+L'idée générale va être de restreindre dramatiquement l'espace des possibilités d'implantation des caméras en imposant qu'elles aient au minimum 2 oeuvres d'art sur leur rayon maximal. On va uniqument garder les positions où la caméra couvre au moins 2 oeuvres, et ce avec une distance maximale pour espérer minimiser les coûts.
+
+Pour trouver ces positions on parcourt l'ensemble des oeuvres d'art. Pour chaque oeuvre, on passe en revue les autres.
+
+- Si la distance entre les deux est supérieure à la distance de la grande caméra, alors on passe à la suivante
+- Si la distance entre les deux est comprise entre la petite distance et la grande distance, alors on peut tracer les cercles de rayon la grande distance, qui passent par les deux oeuvres. Il y en a un de chaque côté de la ligne qui relie les deux oeuvres. On obtient deux possibilités pour des caméras de grande portée
+- Si la distance entre les deux est inférieure à la petite distance, alors on peut tracer un cercle 
+
+On passe de 638,401 positions possibles à 19,952 avec cette méthode. L'algorithme termine en 140s sur un MacBook Air i7@2*1.7GHz, 8Gb RAM
 
 On procède ensuite de manière classique avec SCIP.
 
 Il est nécessaire d'imposer que le rayon des cercles soit légèrement inférieur au rayon théorique pour des raisons d'arrondi.
 
-## Commentaires
-
-Pour une raison assez étrange, le programme génère 20,000 caméras, alors qu'il n'y a que 5,000 oeuvres...
+La solution obtenue coûte 3086.
--- nuit_au_musee/nuit_au_musee.py
+++ nuit_au_musee/nuit_au_musee.py
@@ -2,6 +2,7 @@
 import math
 from time import strftime, gmtime, time
 from pyscipopt import Model, quicksum
+import matplotlib.pyplot as plot
 
 class Solver():
     def __init__(self, filename):
@@ -22,11 +23,18 @@ class Solver():
                     parameters["short_cam_price"] = int(line.strip().split(",")[0])
                     parameters["long_cam_price"] = int(line.strip().split(",")[1])
                 if index > 1:
-                    break
-            for line in file.readlines()[2:]:
-                positions.append(tuple(map(int, line.strip().split(","))))
+                    positions.append(tuple(map(int, line.strip().split(","))))
+
+        # with open("test_results.txt", "w") as end:
+        #     for pos in positions:
+        #         x, y = pos
+        #         end.write(str(x)+','+str(y)+'\n')
+                
         parameters["width"] = int(max([x[1] for x in positions]))
         parameters["height"] = int(max([x[0] for x in positions]))
+
+        print("Map is {} x {}".format(parameters["width"], parameters["height"]))
+        print("There are {} pieces of art".format(len(positions)))
 
         return positions, parameters
 
@@ -36,53 +44,57 @@ class Solver():
         circles = []
         # There is a weird rounding problem that creates empty lists sometimes
         # This offset is here to reduce the circle diameter and be more pessismistic
-        range_offset = 0.01
+        range_offset = 0.0015
         _range = _range - range_offset
+
         dx, dy = x2 - x1, y2 - y1
         q = math.sqrt(dx**2 + dy**2) # distance between points
         x3, y3 = (x1 + x2) / 2, (y1 + y2) / 2 # halfway point
 
         d = math.sqrt(_range**2 - (q / 2)**2)
 
-        if _range + range_offset == self.parameters["short_cam_range"]:
-            c1 = (1, x3 - d * dy / q, y3 + d * dx / q)
-            circles.append(c1)
-            c2 = (1, x3 + d * dy / q, y3 - d * dx / q)
-            circles.append(c2)
-        elif _range + range_offset == self.parameters["long_cam_range"]:
-            c1 = (2, x3 - d * dy / q, y3 + d * dx / q)
-            circles.append(c1)
-            c2 = (2, x3 + d * dy / q, y3 - d * dx / q)
-            circles.append(c2)
+        c1x = x3 - d * dy / q
+        c1y = y3 + d * dx / q
 
-        return circles
+        c2x = x3 + d * dy / q
+        c2y = y3 - d * dx / q
+
+        return c1x, c1y, c2x, c2y
 
     def __get_restricted_positions(self):
         """ Reduce the possible positions with the circle method """
-        camera_positions = []
+        camera_positions = set()
 
         for loc_1_index, loc_1 in enumerate(self.positions):
             x1, y1 = loc_1
-            isolated = True # Account for isolated cameras
+            isolated = True # Account for isolated pieces
             for loc_2_index, loc_2 in enumerate(self.positions):
                 x2, y2 = loc_2
                 if loc_1 == loc_2:
                     continue
                 squared_distance = pow((x2 - x1), 2) + pow((y2 - y1), 2)
-                if squared_distance > pow(self.parameters["long_cam_range"], 2):
+                if squared_distance >= pow(self.parameters["long_cam_range"], 2):
                     # The two locations are too far away
                     continue
-                if squared_distance <= pow(self.parameters["long_cam_range"], 2):
+                if squared_distance < pow(self.parameters["long_cam_range"], 2):
                     # The distance is between the two ranges, only the big one is ok
-                    isolated = False
-                    camera_positions += \
-                        self.__get_centers(x1, y1, x2, y2, self.parameters["long_cam_range"])
-                if squared_distance <= pow(self.parameters["short_cam_range"], 2):
-                    isolated = False
-                    camera_positions += \
-                        self.__get_centers(x1, y1, x2, y2, self.parameters["short_cam_range"])
+                    c1x, c1y, c2x, c2y = self.__get_centers(x1, y1, x2, y2, self.parameters["long_cam_range"])
+                    if c1x >= 0 and c1y >= 0:
+                        isolated = False
+                        camera_positions.add((2, len(camera_positions), c1x, c1y))
+                    if c2x >= 0 and c2y >= 0:
+                        isolated = False
+                        camera_positions.add((2, len(camera_positions), c2x, c2y))
+                if squared_distance < pow(self.parameters["short_cam_range"], 2):
+                    c1x, c1y, c2x, c2y = self.__get_centers(x1, y1, x2, y2, self.parameters["short_cam_range"])
+                    if c1x >= 0 and c1y >= 0:
+                        isolated = False
+                        camera_positions.add((1, len(camera_positions), c1x, c1y))
+                    if c2x >= 0 and c2y >= 0:
+                        isolated = False
+                        camera_positions.add((1, len(camera_positions), c2x, c2y))
             if isolated:
-                camera_positions.append((1, x1, y1))
+                camera_positions.add((1, len(camera_positions), x1, y1))
 
         return camera_positions
 
@@ -91,13 +103,41 @@ class Solver():
         model = Model() # From Pyscipopt
         print("Getting restricted positions for the cameras...")
         camera_positions = self.__get_restricted_positions()
+        print("Found {} possible positions".format(len(camera_positions)))
 
         print("Adding these positions to the model...")
         # Add the cameras
-        X = dict() # Will be indexed with tuples
-        for index, position in enumerate(camera_positions):
-            _type, x, y = position
-            X[_type, index] = model.addVar(str(_type)+','+str(x)+','+str(y), vtype='B')
+        X = {} # Will be indexed with tuples
+        for camera_type, camera_index, x, y in camera_positions:
+            X[camera_type, camera_index] = model.addVar(str(camera_type)+','+str(x)+','+str(y), vtype='B')
+
+        print("Adding the constraints to the model...")
+        # Add the constraints on every piece
+        for piece_index, piece_position in enumerate(self.positions):
+            cameras = set()
+            x1, y1 = piece_position
+            for camera_type, camera_index, x2, y2 in camera_positions:
+                squared_distance = pow((float(x2) - float(x1)), 2) + pow((float(y2) - float(y1)), 2) # Distance to the piece
+
+                if squared_distance >= pow(self.parameters["long_cam_range"], 2):
+                    # The two locations are too far away
+                    continue
+                if squared_distance < pow(self.parameters["long_cam_range"], 2) \
+                    and camera_type == 2:
+                    cameras.add((camera_type, camera_index))
+                if squared_distance < pow(self.parameters["short_cam_range"], 2):
+                    # Note that the camera can be on the piece
+                    cameras.add((camera_type, camera_index))
+            # One camera has to watch the piece
+            if len(cameras) == 0:
+                print("WARNING - Position {},{}".format(x1, y1))
+            if x1 == 301 and y1 == 628:
+                print("Cameras: {}".format(cameras))
+            model.addCons(
+                quicksum(
+                    X[camera_type, camera_index] for (camera_type, camera_index) in cameras
+                ) >= 1
+            )
 
         print("Adding the price constraint...")
         # Define the minimization
@@ -105,31 +145,12 @@ class Solver():
             1: self.parameters["short_cam_price"],
             2: self.parameters["long_cam_price"]
         }
+
         model.setObjective(
             quicksum(
-                X[pos[0], index] * price[pos[0]] for index, pos in enumerate(camera_positions)
+                X[camera_type, camera_index] * price[camera_type] for camera_type, camera_index, x, y in camera_positions
             ), "minimize"
         )
-
-        print("Adding the constraints to the model...")
-        # Add the constraints on every piece
-        for piece_index, piece_position in enumerate(self.positions):
-            x1, y1 = piece_position
-            cameras = []
-            for cam_index, cam_position in enumerate(camera_positions):
-                _type, x2, y2 = cam_position
-                squared_distance = pow((x2 - x1), 2) + pow((y2 - y1), 2)
-                if squared_distance > pow(self.parameters["long_cam_range"], 2):
-                    # The two locations are too far away
-                    continue
-                if squared_distance <= pow(self.parameters["long_cam_range"], 2) \
-                    and _type == 2:
-                    cameras.append((_type, cam_index))
-                if squared_distance <= pow(self.parameters["short_cam_range"], 2) \
-                    and _type == 1: # Note that the camera can be on the piece
-                    cameras.append((_type, cam_index))
-            # One camera has to watch the piece
-            model.addCons(quicksum(X[_type, cam_index] for (_type, cam_index) in cameras) >= 1)
 
         self.model = model
     
@@ -137,7 +158,8 @@ class Solver():
         if self.model:
             with open("submission_{}.txt".format(strftime("%Y-%m-%d", gmtime())), "w") as end:
                 for var in self.model.getVars():
-                    end.write(var.name + "\n")
+                    if self.model.getVal(var):
+                        end.write(var.name + "\n")
 
     def solve(self):
         """ Solve the problem """
@@ -145,13 +167,82 @@ class Solver():
         t1 = time()
         self.positions, self.parameters = self.__parse_file()
         self.__initialize_model()
-        self.model.hideOutput()
+        #self.model.hideOutput()
         self.model.optimize()
         self.create_submission()
         t2 = time()
         print("Completed in {}s".format(t2-t1))
 
+    @staticmethod
+    def square_distance(x1, y1, x2, y2):
+        return (x2 - x1)**2 + (y2 - y1)**2
+
+    def print_solution(self):
+        """ Display the solution """
+        with open("input_9.txt", 'r') as f:
+            pieces_plot = []
+            for piece_position in f.readlines()[2:]:
+                pieces_plot.append(plot.Circle(tuple(map(int, piece_position.strip().split(","))), 0.3, color='r'))
+
+        with open("submission_2018-02-01.txt", 'r') as f:
+            cameras_plot = []
+            for var in f.readlines():
+                _type, x, y = var.strip().split(",")
+                if int(_type) == 1:
+                    cameras_plot.append(plot.Circle((float(x), float(y)), 0.1, color='b'))
+                    cameras_plot.append(plot.Circle((float(x), float(y)), 4, color='b', fill=False))
+                if int(_type) == 2:
+                    cameras_plot.append(plot.Circle((float(x), float(y)), 0.1, color='g'))
+                    cameras_plot.append(plot.Circle((float(x), float(y)), 8, color='g', fill=False))
+
+        fig, ax = plot.subplots()
+        ax.set_xlim(0, 800)
+        ax.set_ylim(0, 800)
+
+        for ppl in pieces_plot:
+            ax.add_artist(ppl)
+
+        for cpl in cameras_plot:
+            ax.add_artist(cpl)
+
+        plot.grid(True)
+        plot.show()
+        fig.savefig('plot_sol.png')
+
+    def test_solution(self, submission):
+        """ Test the solution """
+        self.positions, self.parameters = self.__parse_file()
+        # Get the results
+        with open(submission, 'r') as f:
+            self.results = []
+            for line in f.readlines():
+                line = line.strip()
+                self.results.append([
+                    line.split(",")[0],
+                    line.split(",")[1],
+                    line.split(",")[2]
+                ])
+        
+        for piece_index, piece_position in enumerate(self.positions):
+            x1, y1 = piece_position
+            monitored = False
+            for result in self.results:
+                if result[0] == 1 and \
+                    self.square_distance(x1, y1, result[1], result[2]) <= \
+                        self.parameters["short_cam_range"]**2:
+                    monitored = True
+                    break
+                if result[0] == 2 and \
+                    self.square_distance(x1, y1, result[1], result[2]) <= \
+                        self.parameters["long_cam_range"]**2:
+                    monitored = True
+                    break
+            if not monitored:
+                print("Piece ({}-{}) is not monitored !".format(x1, y1))
+
 
 if __name__ == "__main__":
     solver = Solver("input_9.txt")
     solver.solve()
+    solver.print_solution()
+    #solver.test_solution("submission_2018-02-01.txt")
